c2f348556b82b249d459413d9fd5b553495d79ff
Francis Benyah <francis@oxentec.co.za>
2014-01-31 11:16:45 +0200

    added test invokation for notifications

--- phonegap-workshop/js/main.js
+++ phonegap-workshop/js/main.js
@@ -22,7 +22,10 @@ var app = {
 	},
 
     initialize: function() {
-        this.store = new MemoryStore();
+		var self = this;
+        this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
